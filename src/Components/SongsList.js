2c14070e0fa68067bda486145794899b29deb425
Aruna Turay <tura_arun@yahoo.com>
2023-06-23 13:05:38 -0400

    begin work on fetch request

--- src/Components/SongsList.js
+++ src/Components/SongsList.js
@@ -3,10 +3,21 @@ import { Link } from 'react-router-dom'
 
 
 
-function SongsLIst() {
+function SongsList({ songs }) {
+
+
+  
+
+// const renderSongs = Object.keys(songs).map((songID) => (
+//   <li key={songID}>
+//     <Link to={`/songs/${songID}`}>{songs[songID].}</Link>
+
+//   </li>
+//))
+
   return (
     <div>SongsList</div>
   )
 }
 
-export default SongsLIst
+export default SongsList

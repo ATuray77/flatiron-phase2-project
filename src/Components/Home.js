6d54c7839fabc6920876518bade94adf0e88546c
Aruna Turay <tura_arun@yahoo.com>
2023-07-03 16:20:57 -0400

    updates home page to display song titles

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import "./App.css";
 
-function Home() {
+
+
+const Home = () =>  {
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+  const testDeployedApt = async () => {
+    let apiResults = await fetch("https://new-json-server.onrender.com/songs")
+      .then((r) => r.json())
+      .then((data) => data);
+      console.table(apiResults)
+      setSongs(apiResults)
+  };
+  testDeployedApt();
+   }, []);
+
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <>
+    <h1>Home Page</h1>
+      {songs.map(song => (
+        <p key={song.id}>{song.Title}</p>
+      ))}
+    </>
+  );
+};
 
 export default Home;
